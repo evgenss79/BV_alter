6b8653a5aa7001d531ff9ded72186b8ef578a23d
evgenss79 <evgenss79@gmail.com>
2025-11-28 19:54:21 +0100

    Create includes/header.php with navigation header

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<?php
+// navigation header
+?>
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
