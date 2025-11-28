23187eede3cd6f6f96ce99bc0b955d015c6cff52
evgenss79 <evgenss79@gmail.com>
2025-11-28 21:19:35 +0100

    Delete blog-post.php

--- blog-post.php
+++
@@ -1,6 +0,0 @@
-<?php include __DIR__ . '/templates/header.php'; ?>
-<section class="section">
-    <h1><?php echo I18N::tPage('blog.post_title'); ?></h1>
-    <p><?php echo nl2br(I18N::tPage('blog.post_body')); ?></p>
-</section>
-<?php include __DIR__ . '/templates/footer.php'; ?>
